27922ed00bf346ec73f0d24aa969ae12b88c9cc9
Gerald <gengbin@91bihu.com>
2020-06-11 18:05:12 +0800

    update files

--- Pointer.c
+++ Pointer.c
@@ -1,27 +1,98 @@
 #include <stdio.h>
 
+void AAA(int *p)
+{
 
-
-void AAA(int *p){
-
-     *p=10;
+    *p = 10;
 }
 
+void BBB(int **p)
+{
+
+    **p = 20;
+}
+
+// int main0()
+// {
+
+//     int i = 0;
+//     int *p = NULL;
+//     p = &i;
+//     *p = 5;
+//     printf("i=%d\n", i);
+
+//     AAA(&i);
+//     printf("i=%d\n", i);
+
+//     BBB(&p);
+//     printf("i=%d", i);
+//     return 0;
+// }
+
+void CCC(char** p)
+{
+
+    char *temp = NULL;
+    temp = (char*) malloc(100);
+    *p = temp;
+}
 
 int main()
 {
 
-    int i = 0;
-    int*  p=NULL;
-    p=&i;
-    *p=5;   
-    printf("i=%d\n",i);
-
-
-
-    AAA(&i);
-     printf("i=%d",i);
-
-
+    char* p = NULL;
+char  **p1=NULL;
+p=0x11;
+p1=&p;
+*p1=0x22;
+    printf("p=%x\n", p);
+    CCC(&p);
+    printf("p=%x\n", p);
     return 0;
 }
+
+
+
+void DDD(char *p){
+char  * temp=NULL;
+temp=(char*)malloc(100);
+*p=temp;
+
+}
+
+// int main2(){
+// char   * p=NULL;
+// char   *p1=NULL;
+// p=0x11;
+// p1=&p;
+// *p1=0x22;
+// printf("p=%x\n",p);
+// DDD(&p);
+// printf("p=%x\n",p);
+// return 0;
+// }
+
+
+
+
+// int main3(){
+
+// int  i=0;
+// int *  p=NULL;
+// p=&i;
+// *p=10;
+// printf("p=%d\n",i);
+// // error
+// // int  *p1=NULL;
+// // p1=&p;
+// // **p1=20;
+// // printf("p=%d\n",i);
+
+// int  **p1=NULL;
+// p1=&p;
+// **p1=20;
+// printf("p=%d\n",i);
+
+// return 0;
+
+// }
